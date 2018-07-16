0c49805a0bb10be15780d7d79ac2654b632b0398
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-07-16 17:50:46 -0400

    everything finished through 8

---
+++ liri.js
@@ -0,0 +1,20 @@
+// js
+require("dotenv").config();
+
+// Spotify API keys
+var spotID = process.env.SPOTIFY_ID;
+var spotSecret = process.env.SPOTIFY_SECRET;
+
+console.log(spotID);
+console.log(spotSecret);
+
+// Twitter API keys
+var twitConKey = process.env.TWITTER_CONSUMER_KEY;
+var twitConSecret = process.env.TWITTER_CONSUMER_SECRET;
+var twitTokenKey = process.env.TWITTER_ACCESS_TOKEN_KEY;
+var twitTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+
+console.log(twitConKey);
+console.log(twitConSecret);
+console.log(twitTokenKey);
+console.log(twitTokenSecret);
